97b876c2190bac1c64dea86ec69c9a8f803f34d1
Debora Ochoa <ochoadeb@yahoo.com>
2021-05-30 22:49:30 -0300

    Principio ejercicio con dudas miles

--- monitor.py
+++ monitor.py
@@ -3,15 +3,10 @@
 import subprocess
 
 hostname = subprocess.run(["hostname"], capture_output=True, encoding='utf-8')
-
 var_space = subprocess.run(["df","-h","/var"], capture_output=True, encoding='utf-8')
-
 mem_space = subprocess.run(["free","thm"], capture_output=True, encoding='utf-8')
-#query = "ps -ef"
-#cant_proc = subprocess.run([query], capture_output=True, encoding='utf-8')
-
+cant_proc = subprocess.run(["ps","-ef"], capture_output=True, encoding='utf-8') #no se como poner el | wc -l
 cant_cpu = subprocess.run(["getconf","_NPROCESSORS_ONLN"], capture_output=True, encoding='utf-8')
-
 load_avg = subprocess.run(["uptime"], capture_output=True, encoding='utf-8')
 
 print("******** Hostname *************")
@@ -21,8 +16,12 @@ print(var_space.stdout)
 print("******** memoria libre *************")
 print(mem_space.stdout)
 print("********* Procesos ************")
-#print(cant_proc.stdout)
+print(cant_proc)
 print("********* CPUs ************")
 print(cant_cpu.stdout)
 print("********* Load Agerage ************")
 print(load_avg.stdout)
+
+#Consultar:
+#Como contatenar comandos si tengo un |
+#como llevarme el valor que quiero de la salida de un comando. 
